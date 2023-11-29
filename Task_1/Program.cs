217ed8e0af324bf4b1848779ac010e9314b0a863
NatashaAzyamova <azyamova@bk.ru>
2023-11-29 14:37:25 +0300

    готово

---
+++ Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Урок 3. Массивы
+// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+Random random = new Random();
+// Создание массива из 10 элементов
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(1, 101); // заполняем числами от 1 до 100. 
+}
+    for(int i = 0; i < array.Length; i++)
+{
+Console.Write($"{array[i] + " "}");
+}
+int count = 0;
+for (int i = 0; i < array.Length; i++) 
+{
+if ((array[i] >= 20) && (array[i] <= 90)) count++; 
+}
+Console.WriteLine();
+Console.WriteLine("Количество элементов в отрезке [20,90]: " + count);
---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Задайте массив на 10 целых чисел. 
+// Напишите программу, которая определяет количество чётных чисел в массиве.
+Random random = new Random();
+// Создание массива из 10 элементов
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(1, 900); // заполняем числами
+}
+    for(int i = 0; i < array.Length; i++)
+{
+Console.Write($"{array[i] + " "}");
+}
+int count = 0;
+for (int i = 0; i < array.Length; i++) 
+{
+if (array[i] % 2 == 0) count++; 
+}
+Console.WriteLine();
+Console.WriteLine("Количество чётных чисел в массиве: " + count);
